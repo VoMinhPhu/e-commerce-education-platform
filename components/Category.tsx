527faa53a2ab8ff02ccaf8856e18d6848a640392
vominhphu <vominhphu2003@gmail.com>
2025-07-10 00:35:20 +0700

    UPDATE : responsive category

--- components/Category.tsx
+++ components/Category.tsx
@@ -5,33 +5,33 @@ const Category = () => {
   return (
     <div className="flex items-center justify-center py-8 w-full bg-primary-foreground">
       <div className="max-w-[1200px] border rounded-md w-full bg-white">
-        <p className="font-medium text-3xl text-primary/60 mt-2 ml-2">
+        <p className="font-medium text-2xl md:text-3xl text-primary/60 mt-2 ml-2">
           PRODUCT CATEGORY
         </p>
-        <div className="p-12 grid grid-cols-2 w-full">
-          <div className="flex justify-center">
-            <Card className="rounded-md w-3/4 p-8 cursor-pointer">
-              <CardContent className="flex items-center justify-center gap-8">
+        <div className="p-6 lg:p-12 grid grid-cols-2 w-full">
+          <div className="flex justify-center gap-2">
+            <Card className="rounded-md md:w-3/4 md:p-8 cursor-pointer">
+              <CardContent className="flex items-center justify-center gap-2 md:gap-8">
                 <Image
                   src="/category/course.svg"
                   alt="course"
                   width={40}
                   height={40}
                 />
-                <p className="text-lg font-medium">Courses</p>
+                <p className="text-lg font-medium gap-1">Courses</p>
               </CardContent>
             </Card>
           </div>
           <div className="flex justify-center">
-            <Card className="rounded-md w-3/4 p-8 cursor-pointer">
-              <CardContent className="flex items-center justify-center gap-8">
+            <Card className="rounded-md md:w-3/4 md:p-8 cursor-pointer">
+              <CardContent className="flex items-center justify-center gap-2 md:gap-8">
                 <Image
                   src="/category/books.svg"
                   alt="books"
                   width={40}
                   height={40}
                 />
-                <p className="text-lg font-medium">Books</p>
+                <p className="text-lg font-medium gap-1">Books</p>
               </CardContent>
             </Card>
           </div>
