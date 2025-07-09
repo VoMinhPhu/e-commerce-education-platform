eb2911d2cb0d96865dc37112517b138a83ba6995
vominhphu <vominhphu2003@gmail.com>
2025-07-10 01:02:04 +0700

    UPDATE: responsive banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -27,45 +27,67 @@ const Banner = () => {
   }, [api]);
   return (
     <div className="flex justify-center p-8">
-      <div className="grid grid-cols-3 max-w-[1200px] w-full">
-        <div className="col-span-2">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 max-w-[1200px] w-full">
+        <div className="md:col-span-2">
           <Carousel
             opts={{
               loop: true,
             }}
             setApi={setApi}
+            className="h-full"
           >
-            <CarouselContent className="w-full h-110">
+            <CarouselContent className="h-full">
               {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem key={index} className="pl-4.5">
+                <CarouselItem
+                  key={index}
+                  className="flex items-center justify-center w-full h-full md:pl-4.5"
+                >
                   <Image
                     src={`/banner/banner${index + 1}.png`}
                     alt={`banner ${index + 1}`}
                     width={450}
                     height={200}
-                    objectFit="fill"
-                    className="w-full h-110 rounded-[3px]"
+                    objectFit="cover"
+                    className="w-full h-full md:h-110 rounded-[3px]"
                   />
                 </CarouselItem>
               ))}
             </CarouselContent>
           </Carousel>
         </div>
-        <div className="gap-3 grid grid-cols-1 items-center">
-          <Image
+        <div className="gap-3 hidden md:grid grid-cols-1 items-center h-110">
+          {/* <Image
             src="/banner/banner1.png"
             alt="banner 1"
             width={450}
             height={200}
-            className="object-cover h-52 rounded-[3px]"
+            className="rounded-[3px]"
           />
           <Image
             src="/banner/banner3.png"
             alt="banner 3"
             width={450}
             height={200}
-            className="object-cover h-52 rounded-[3px]"
-          />
+            className="rounded-[3px]"
+          /> */}
+          <div className="flex">
+            <Image
+              src="/banner/banner1.png"
+              alt="banner 1"
+              width={420}
+              height={200}
+              className="rounded-[3px] h-52.75"
+            />
+          </div>
+          <div className="flex">
+            <Image
+              src="/banner/banner3.png"
+              alt="banner 3"
+              width={420}
+              height={200}
+              className="rounded-[3px] h-52.75"
+            />
+          </div>
         </div>
       </div>
     </div>
