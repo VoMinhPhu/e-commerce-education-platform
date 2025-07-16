fa7e488dca0d1e55651f3a6a7396fb13c4e3a092
vominhphu <vominhphu2003@gmail.com>
2025-07-16 11:09:31 +0700

    UPDATE: responsive auth page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 
 import {
   Card,
@@ -17,13 +18,20 @@ const page = () => {
   return (
     <div className="mt-16 bg-primary-foreground">
       <div className="max-w-300 mx-auto grid grid-cols-2">
-        <div>1</div>
-        <div className="p-16">
+        <div className="p-4 lg:p-16 pr-0 hidden md:grid justify-center">
+          <div className="flex items-center justify-center">
+            <Image src={"/logo.svg"} alt="logo" width={250} height={250} />
+          </div>
+          <p className="text-center text-xl text-muted-foreground">
+            Empowering learners to thrive in the digital commerce world.
+          </p>
+        </div>
+        <div className="p-4 lg:p-16 col-span-2 md:col-span-1">
           <Card className="w-full max-w-sm rounded-sm">
             <CardHeader>
-              <CardTitle>Login to your account</CardTitle>
+              <CardTitle>Register your account</CardTitle>
               <CardDescription>
-                Enter your username below to login to your account
+                Enter your username below to register your account
               </CardDescription>
               <CardAction>
                 <Link href={"/login"}>
@@ -36,6 +44,12 @@ const page = () => {
             <CardContent>
               <FormRegister />
               <Button variant="outline" className="w-full rounded-sm mt-2">
+                <Image
+                  src={"/google.svg"}
+                  alt="google icon"
+                  width={16}
+                  height={16}
+                />
                 Login with Google
               </Button>
             </CardContent>
